8b0e2961da015b36b450fb431621f913c8e7498f
Friox <devfriox@gmail.com>
2025-06-13 22:38:44 +0900

    feat: 정기권 엔티티 추가 (goorm-sudo/raillo-backend#1)

--- src/main/java/com/sudo/railo/ticket/domain/SeasonTicket.java
+++ src/main/java/com/sudo/railo/ticket/domain/SeasonTicket.java
@@ -1,6 +1,5 @@
 package com.sudo.railo.ticket.domain;
 
-import com.sudo.railo.member.domain.Member;
 import jakarta.persistence.*;
 import lombok.AccessLevel;
 import lombok.Getter;
@@ -8,34 +7,35 @@ import lombok.NoArgsConstructor;
 
 import java.time.LocalDate;
 
-
 @Entity
 @Getter
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 public class SeasonTicket {
 
-    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Long id;
-
-    @Enumerated(EnumType.STRING)
-    private TicketType ticketType;
-
-    @Column(nullable = false)
-    private LocalDate StartAt;
-
-    private LocalDate EndAt;
-
-    @Column(nullable = false, length = 1)
-    private String isHolidayUsable;
-
-    @Enumerated(EnumType.STRING)
-    private SeasonStatus seasonStatus;
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long seasonTicketId;
 
     @OneToOne
     @JoinColumn(name = "qr_id", unique = true)
-    private Qr qr;
+    private Qr qrId;
 
-    @ManyToOne(fetch = FetchType.LAZY)
-    @JoinColumn(name = "member_id")
-    private Member member;
+    // TODO: 멤버 id
+
+    @Enumerated(EnumType.STRING)
+    @Column(nullable = false)
+    private TicketType ticketType;
+
+    @Column(nullable = false)
+    private LocalDate startAt;
+
+    private LocalDate endAt;
+
+    @Column(nullable = false)
+    private boolean isHolidayUsable;
+
+    @Enumerated(EnumType.STRING)
+    @Column(nullable = false)
+    private SeasonTicketStatus seasonTicketStatus;
+
 }
