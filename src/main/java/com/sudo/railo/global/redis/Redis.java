bc36b8390cc24afb3281cab76000f55d40c0fdd3
Jimin730 <dnddnd120@naver.com>
2025-06-18 14:32:09 +0900

    feat: 레디스 초기 설정 파일 추가

---
+++ src/main/java/com/sudo/railo/global/redis/Redis.java
@@ -0,0 +1,20 @@
+package com.sudo.railo.global.redis;
+
+import org.springframework.data.redis.core.RedisHash;
+
+import jakarta.persistence.Id;
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
+
+@RedisHash(value = "MemberToken", timeToLive = 3600 * 24 * 14)
+@Getter
+@Setter
+@AllArgsConstructor(access = AccessLevel.PROTECTED)
+public class Redis {
+
+	@Id
+	private String memberNo;
+	private String refreshToken;
+}
