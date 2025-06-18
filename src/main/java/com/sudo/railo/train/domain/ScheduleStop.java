2ef1d8ef5edbf80cc48a375f1e095588c65520c2
Ogu1208 <kdasunb6@gmail.com>
2025-06-19 03:08:28 +0900

    feat: seat엔티티의 행,  열 컬럼 이름 변경, comment 추가 (goorm-sudo/raillo-backend#33)

--- src/main/java/com/sudo/railo/train/domain/ScheduleStop.java
+++ src/main/java/com/sudo/railo/train/domain/ScheduleStop.java
@@ -1,33 +1,38 @@
 package com.sudo.railo.train.domain;
 
-import jakarta.persistence.*;
+import java.time.LocalTime;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
 import lombok.AccessLevel;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
-
-import java.time.LocalTime;
 
 @Entity
 @Getter
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 public class ScheduleStop {
 
-    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Long id;
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
 
-    private int stopOrder;
+	private int stopOrder;
 
-    private LocalTime arrivalTime;
+	private LocalTime arrivalTime;
 
-    private LocalTime departureTime;
+	private LocalTime departureTime;
 
-    @ManyToOne(fetch = FetchType.LAZY)
-    @JoinColumn(name = "train_schedule_id")
-    private TrainSchedule trainSchedule;
+	@ManyToOne(fetch = FetchType.LAZY)
+	@JoinColumn(name = "train_schedule_id")
+	private TrainSchedule trainSchedule;
 
-    @ManyToOne(fetch = FetchType.LAZY)
-    @JoinColumn(name = "station_id")
-    private Station station;
-
-
+	@ManyToOne(fetch = FetchType.LAZY)
+	@JoinColumn(name = "station_id")
+	private Station station;
 }
--- src/main/java/com/sudo/railo/train/domain/Seat.java
+++ src/main/java/com/sudo/railo/train/domain/Seat.java
@@ -1,6 +1,17 @@
 package com.sudo.railo.train.domain;
 
-import jakarta.persistence.*;
+import org.hibernate.annotations.Comment;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
 import lombok.AccessLevel;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -10,25 +21,27 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 public class Seat {
 
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Long id;
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
 
-    private int seatNumber;
+	@Comment("좌석 행 (1, 2, 3, 4)")
+	private int seatRow;
 
-    @Column(length = 1)
-    private String seatRow;
+	@Column(length = 1)
+	@Comment("좌석 열 문자(A, B, C, D)")
+	private String seatColumn;
 
-    @Enumerated(EnumType.STRING)
-    private SeatType seatType;
+	@Enumerated(EnumType.STRING)
+	private SeatType seatType;
 
-    @Column(length = 1)
-    private String isAccessible;
+	@Column(length = 1)
+	private String isAccessible;
 
-    @Column(length = 1)
-    private String isAvailable;
+	@Column(length = 1)
+	private String isAvailable;
 
-    @ManyToOne(fetch = FetchType.LAZY)
-    @JoinColumn(name = "train_car_id")
-    private TrainCar trainCar;
+	@ManyToOne(fetch = FetchType.LAZY)
+	@JoinColumn(name = "train_car_id")
+	private TrainCar trainCar;
 }
