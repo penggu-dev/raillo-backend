7b99593d979a5f441018d9703525004198f11d2c
Jimin730 <dnddnd120@naver.com>
2025-06-19 00:09:34 +0900

    refactor: SecurityConfig 파일 수정 (goorm-sudo/raillo-backend#16)

--- src/main/java/com/sudo/railo/global/security/SecurityConfig.java
+++ src/main/java/com/sudo/railo/global/security/SecurityConfig.java
@@ -1,34 +1,51 @@
 package com.sudo.railo.global.security;
 
-import lombok.RequiredArgsConstructor;
+import static org.springframework.security.config.http.SessionCreationPolicy.*;
+
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 
-import static org.springframework.security.config.http.SessionCreationPolicy.STATELESS;
+import lombok.RequiredArgsConstructor;
 
 @Configuration
 @RequiredArgsConstructor
 @EnableWebSecurity
 public class SecurityConfig {
 
-    // 필터 설정
-    @Bean
-    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
-        return http
-                // CSRF 보호 비활성화
-                .csrf(AbstractHttpConfigurer::disable)
-                // HTTP 요청에 대한 접근 권한 설정 (초기 모두 허용)
-                .authorizeHttpRequests(auth -> {
-                    auth.anyRequest().permitAll();
-                })
-                // 세션 방식을 사용하지 않음
-                .sessionManagement(session -> session.sessionCreationPolicy(STATELESS))
-                // 기본 CORS 설정 사용
-                .cors(cors -> {})
-                .build();
-    }
+	@Bean
+	public BCryptPasswordEncoder passwordEncoder() {
+		return new BCryptPasswordEncoder();
+	}
+
+	// 아이디와 패스워드의 일치 여부를 확인할 때 사용하는 객체
+	@Bean
+	public AuthenticationManager authenticationManagerBean(AuthenticationConfiguration config) throws Exception {
+		return config.getAuthenticationManager();
+	}
+
+	@Bean
+	public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+		return http
+			.csrf(AbstractHttpConfigurer::disable)
+			.sessionManagement(session -> session.sessionCreationPolicy(STATELESS))
+			// 기본 CORS 설정 사용
+			.cors(cors -> {
+			})
+			// HTTP 요청에 대한 접근 권한 설정
+			// 로그인 구현 전까지 모두 허용 예정
+			.authorizeHttpRequests(auth -> {
+				auth.requestMatchers("/auth/**").permitAll()
+					//swagger API 문서 허용
+					.requestMatchers("/v3/api-docs/**", "/swagger-ui/**", "/swagger-ui.html").permitAll()
+					.anyRequest().permitAll();
+			})
+			.build();
+	}
 }
