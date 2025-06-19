6c368163202ff763ab9df553b9288e9ce66683de
Jimin730 <dnddnd120@naver.com>
2025-06-19 13:18:54 +0900

    refactor: 회원번호 발급 로직 수정 (goorm-sudo/raillo-backend#16)

--- src/main/java/com/sudo/railo/member/application/MemberNoGenerator.java
+++ src/main/java/com/sudo/railo/member/application/MemberNoGenerator.java
@@ -1,7 +1,10 @@
 package com.sudo.railo.member.application;
 
-import java.time.Duration;
+import java.time.Instant;
 import java.time.LocalDate;
+import java.time.LocalTime;
+import java.time.ZoneId;
+import java.time.ZonedDateTime;
 import java.time.format.DateTimeFormatter;
 
 import org.springframework.data.redis.core.StringRedisTemplate;
@@ -16,6 +19,7 @@ public class MemberNoGenerator {
 	private static final DateTimeFormatter DATE_FORMATTER = DateTimeFormatter.ofPattern("yyyyMMdd");
 
 	private final StringRedisTemplate redisTemplate;
+	private static final ZoneId ZONE_ID = ZoneId.of("Asia/Seoul");
 
 	public String generateMemberNo() {
 		String today = LocalDate.now().format(DATE_FORMATTER);
@@ -23,7 +27,12 @@ public class MemberNoGenerator {
 
 		Long counter = redisTemplate.opsForValue().increment(redisKey);
 
-		redisTemplate.expire(redisKey, Duration.ofDays(1));
+		// 자정까지 남은 시간 계산
+		ZonedDateTime midnightToday = ZonedDateTime.of(LocalDate.now(ZONE_ID), LocalTime.MAX, ZONE_ID);
+		Instant midnightInstant = midnightToday.toInstant();
+
+		// 자정 만료
+		redisTemplate.expireAt(redisKey, midnightInstant);
 
 		String paddedCounter = String.format("%04d", counter);
 
