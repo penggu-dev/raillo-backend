1ea99690a17fb3950ab22dcd9a2b43cb5d3208f9
Friox <devfriox@gmail.com>
2025-06-26 11:44:41 +0900

    feat: 예약 관련 변수값 클래스 추가 (goorm-sudo/raillo-backend#35)

---
+++ src/main/java/com/sudo/railo/booking/config/BookingConfig.java
@@ -0,0 +1,20 @@
+package com.sudo.railo.booking.config;
+
+import org.springframework.boot.context.properties.ConfigurationProperties;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@Getter
+@AllArgsConstructor
+@ConfigurationProperties(prefix = "booking")
+public class BookingConfig {
+
+	private final Expiration expiration;
+
+	@Getter
+	@AllArgsConstructor
+	public static class Expiration {
+		private final Integer reservation;
+	}
+}
