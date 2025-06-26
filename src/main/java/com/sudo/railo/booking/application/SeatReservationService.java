5f4f29cd41ca50b17a5821fe483b2193886f375b
Friox <devfriox@gmail.com>
2025-06-26 11:43:16 +0900

    feat: 예외처리 로직 수정, 변수 분리, 만료좌석 일괄취소 로직 수정 (goorm-sudo/raillo-backend#35)

--- src/main/java/com/sudo/railo/booking/application/SeatReservationService.java
+++ src/main/java/com/sudo/railo/booking/application/SeatReservationService.java
@@ -1,11 +1,10 @@
 package com.sudo.railo.booking.application;
 
 import java.time.LocalDateTime;
-import java.util.List;
-import java.util.Optional;
 
 import org.springframework.stereotype.Service;
 
+import com.sudo.railo.booking.config.BookingConfig;
 import com.sudo.railo.booking.domain.SeatReservation;
 import com.sudo.railo.booking.exception.BookingError;
 import com.sudo.railo.booking.infra.SeatReservationRepository;
@@ -19,64 +18,69 @@ import lombok.RequiredArgsConstructor;
 @RequiredArgsConstructor
 public class SeatReservationService {
 
-    private final SeatReservationRepository seatReservationRepository;
+	private final SeatReservationRepository seatReservationRepository;
+	private final BookingConfig bookingConfig;
 
-    /***
-     * 좌석을 예약하는 메서드
-     * @param trainScheduleId - 열차 스케줄 ID
-     * @param seatId - 좌석 ID
-     */
-    @Transactional
-    public void reserveSeat(Long trainScheduleId, Long seatId) {
-        try {
-            Optional<SeatReservation> result = seatReservationRepository.findByTrainScheduleIdAndSeatId(trainScheduleId, seatId);
-            if (!result.isPresent()) {
-                throw new BusinessException(BookingError.SEAT_NOT_FOUND);
-            }
-            SeatReservation seatReservation = result.get();
-            seatReservation.reserveSeat();
-            seatReservationRepository.save(seatReservation);
-        } catch (OptimisticLockException e) {
-            // 동시성 문제 발생
-            throw new BusinessException(BookingError.SEAT_ALREADY_RESERVED);
-        } catch (Exception e) {
-            throw new BusinessException(BookingError.SEAT_RESERVATION_FAILED);
-        }
-    }
+	/***
+	 * 좌석을 예약하는 메서드
+	 * @param trainScheduleId - 열차 스케줄 ID
+	 * @param seatId - 좌석 ID
+	 */
+	@Transactional
+	public void reserveSeat(Long trainScheduleId, Long seatId) {
+		try {
+			SeatReservation seatReservation = seatReservationRepository.findByTrainScheduleIdAndSeatId(trainScheduleId,
+					seatId)
+				.orElseThrow(() -> new BusinessException(BookingError.SEAT_NOT_FOUND));
+			seatReservation.reserveSeat();
+			seatReservationRepository.save(seatReservation);
+		} catch (OptimisticLockException e) {
+			// 동시성 문제 발생
+			throw new BusinessException(BookingError.SEAT_ALREADY_RESERVED);
+		} catch (BusinessException e) {
+			// BusinessException은 그대로 넘기기
+			throw e;
+		} catch (Exception e) {
+			throw new BusinessException(BookingError.SEAT_RESERVATION_FAILED);
+		}
+	}
 
-    /***
-     * 좌석 예약을 취소하는 메서드
-     * @param trainScheduleId - 열차 스케줄 ID
-     * @param seatId - 좌석 ID
-     */
-    @Transactional
-    public void cancelReservation(Long trainScheduleId, Long seatId) {
-        try {
-            Optional<SeatReservation> result = seatReservationRepository.findByTrainScheduleIdAndSeatId(trainScheduleId, seatId);
-            if (!result.isPresent()) {
-                throw new BusinessException(BookingError.SEAT_NOT_FOUND);
-            }
-            SeatReservation seatReservation = result.get();
-            seatReservation.cancelReservation();
-            seatReservationRepository.save(seatReservation);
-        } catch (OptimisticLockException e) {
-            // 동시성 문제 발생
-            throw new BusinessException(BookingError.SEAT_ALREADY_CANCELLED);
-        } catch (Exception e) {
-            throw new BusinessException(BookingError.SEAT_CANCELLATION_FAILED);
-        }
-    }
+	/***
+	 * 좌석 예약을 취소하는 메서드
+	 * @param trainScheduleId - 열차 스케줄 ID
+	 * @param seatId - 좌석 ID
+	 */
+	@Transactional
+	public void cancelReservation(Long trainScheduleId, Long seatId) {
+		try {
+			SeatReservation seatReservation = seatReservationRepository.findByTrainScheduleIdAndSeatId(trainScheduleId,
+					seatId)
+				.orElseThrow(() -> new BusinessException(BookingError.SEAT_NOT_FOUND));
+			seatReservation.cancelReservation();
+			seatReservationRepository.save(seatReservation);
+		} catch (OptimisticLockException e) {
+			// 동시성 문제 발생
+			throw new BusinessException(BookingError.SEAT_ALREADY_CANCELLED);
+		} catch (BusinessException e) {
+			// BusinessException은 그대로 넘기기
+			throw e;
+		} catch (Exception e) {
+			throw new BusinessException(BookingError.SEAT_CANCELLATION_FAILED);
+		}
+	}
 
-    /***
-     * 예약 만료 시간을 기준으로 만료된 좌석을 취소하는 메서드
-     */
-    @Transactional
-    public void cancelExpiredReservation() {
-        LocalDateTime expiredAt = LocalDateTime.now().minusMinutes(10);
-        List<SeatReservation> expiredSeats = seatReservationRepository.findExpiredSeats(expiredAt);
-        for (SeatReservation seatReservation : expiredSeats) {
-            seatReservation.cancelReservation();
-            seatReservationRepository.save(seatReservation);
-        }
-    }
+	/***
+	 * 예약 만료 시간을 기준으로 만료된 좌석을 취소하는 메서드
+	 */
+	@Transactional
+	public void cancelExpiredReservation() {
+		LocalDateTime expiredAt = LocalDateTime.now().minusMinutes(bookingConfig.getExpiration().getReservation());
+		try {
+			seatReservationRepository.cancelExpiredSeats(expiredAt);
+		} catch (BusinessException e) {
+			throw e;
+		} catch (Exception e) {
+			throw new BusinessException(BookingError.EXPIRED_SEAT_CANCELLATION_FAILED);
+		}
+	}
 }
