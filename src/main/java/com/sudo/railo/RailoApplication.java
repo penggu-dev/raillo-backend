6fefe51fb13b3fb4ea0dd7b85c1c887b2821fede
Friox <devfriox@gmail.com>
2025-06-26 11:36:39 +0900

    feat: ConfigurationPropertiesScan 활성화 (goorm-sudo/raillo-backend#35)

--- src/main/java/com/sudo/railo/RailoApplication.java
+++ src/main/java/com/sudo/railo/RailoApplication.java
@@ -2,14 +2,16 @@ package com.sudo.railo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
 @EnableJpaAuditing
+@ConfigurationPropertiesScan
 public class RailoApplication {
 
-    public static void main(String[] args) {
-        SpringApplication.run(RailoApplication.class, args);
-    }
+	public static void main(String[] args) {
+		SpringApplication.run(RailoApplication.class, args);
+	}
 
 }
