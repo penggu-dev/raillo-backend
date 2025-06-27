43c9732f13aa8ec45f4b5009f9bf18d9b8d36d6b
Jimin730 <dnddnd120@naver.com>
2025-06-27 16:58:23 +0900

    feat: Swagger api 코드 추가 (goorm-sudo/raillo-backend#42)

--- src/main/java/com/sudo/railo/member/docs/AuthControllerDocs.java
+++ src/main/java/com/sudo/railo/member/docs/AuthControllerDocs.java
@@ -4,6 +4,7 @@ import com.sudo.railo.global.exception.error.ErrorResponse;
 import com.sudo.railo.global.success.SuccessResponse;
 import com.sudo.railo.member.application.dto.request.MemberNoLoginRequest;
 import com.sudo.railo.member.application.dto.request.SignUpRequest;
+import com.sudo.railo.member.application.dto.response.ReissueTokenResponse;
 import com.sudo.railo.member.application.dto.response.SignUpResponse;
 import com.sudo.railo.member.application.dto.response.TokenResponse;
 
@@ -41,4 +42,12 @@ public interface AuthControllerDocs {
 		@ApiResponse(responseCode = "403", description = "이미 로그아웃된 토큰입니다.", content = @Content(schema = @Schema(implementation = ErrorResponse.class)))
 	})
 	SuccessResponse<?> logout(HttpServletRequest request);
+
+	@Operation(method = "POST", summary = "accessToken 재발급", description = "accessToken 이 만료되었을 때, 토큰을 재발급 받을 수 있도록 합니다.",
+		security = {@SecurityRequirement(name = "bearerAuth")})
+	@ApiResponses(value = {
+		@ApiResponse(responseCode = "200", description = "accessToken 이 성공적으로 재발급되었습니다."),
+		@ApiResponse(responseCode = "403", description = "유효하지 않은 토큰입니다.", content = @Content(schema = @Schema(implementation = ErrorResponse.class)))
+	})
+	SuccessResponse<ReissueTokenResponse> reissue(HttpServletRequest request);
 }
