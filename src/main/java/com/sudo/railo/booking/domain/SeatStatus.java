961068444de4001ffd1c8f8aa833ecac3cba9816
Ogu1208 <kdasunb6@gmail.com>
2025-06-28 18:13:46 +0900

    feat: SeatStatus에서 selecting 상태 삭제 (goorm-sudo/raillo-backend#5)

--- src/main/java/com/sudo/railo/booking/domain/SeatStatus.java
+++ src/main/java/com/sudo/railo/booking/domain/SeatStatus.java
@@ -1,5 +1,14 @@
 package com.sudo.railo.booking.domain;
 
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+
+@Getter
+@RequiredArgsConstructor
 public enum SeatStatus {
-    AVAILABLE, SELECTING, RESERVED, LOCKED
+	AVAILABLE("예약가능"),
+	RESERVED("예매 완료"),
+	LOCKED("잠금 상태");
+
+	private final String description;
 }
