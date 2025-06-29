bf2c0c7fd7a120f2dc959c07f63e08a5a96e0928
조윤성 <yunsung1206@gmail.com>
2025-06-29 18:08:56 +0900

    feat: 운임표 파싱 및 저장 로직 구현 (goorm-sudo/raillo-backend#31)

--- src/main/java/com/sudo/railo/train/application/StationFareCreator.java
+++ src/main/java/com/sudo/railo/train/application/StationFareCreator.java
@@ -1,12 +1,20 @@
 package com.sudo.railo.train.application;
 
+import java.util.ArrayList;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Map;
+import java.util.Set;
+import java.util.stream.Stream;
 
 import org.apache.poi.ss.usermodel.Sheet;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import com.sudo.railo.train.application.dto.excel.StationFareData;
 import com.sudo.railo.train.application.dto.excel.StationFareHeader;
+import com.sudo.railo.train.domain.Station;
+import com.sudo.railo.train.domain.StationFare;
 import com.sudo.railo.train.infrastructure.StationFareRepository;
 import com.sudo.railo.train.infrastructure.excel.StationFareParser;
 
@@ -24,13 +32,55 @@ public class StationFareCreator {
 
 	@Transactional
 	public void createStationFare() {
-		log.info("운임표 생성 시작");
+		try {
+			log.info("운임표 생성 시작");
 
-		List<Sheet> sheets = parser.getSheets();
-		sheets.forEach(sheet -> {
-			StationFareHeader header = parser.getHeader(sheet);
-		});
+			List<Sheet> sheets = parser.getSheets();
+			Set<StationFareData> stationFareData = new HashSet<>();
+			sheets.forEach(sheet -> {
+				log.info("{} 시트 파싱 시작", sheet.getSheetName());
 
-		log.info("운임표 생성 완료");
+				StationFareHeader header = parser.getHeader(sheet);
+				List<StationFareData> data = parser.getStationFareData(sheet, header);
+				stationFareData.addAll(data);
+
+				log.info("{} 시트 파싱 종료", sheet.getSheetName());
+			});
+
+			persistStationFare(stationFareData);
+
+			log.info("운임표 생성 완료");
+		} catch (Exception ex) {
+			log.error("운임표 생성 중 예외가 발생했습니다", ex);
+		}
+	}
+
+	private void persistStationFare(Set<StationFareData> stationFareData) {
+		List<String> stationNames = stationFareData.stream()
+			.flatMap(data -> Stream.of(data.departureStation(), data.arrivalStation()))
+			.distinct()
+			.toList();
+		Map<String, Station> stations = stationService.findOrCreateStation(stationNames);
+
+		List<StationFare> stationFares = new ArrayList<>();
+		stationFares.addAll(stationFareData.stream()
+			.map(data -> StationFare.create(
+				stations.get(data.departureStation()),
+				stations.get(data.arrivalStation()),
+				data.standardFare(),
+				data.firstClassFare()
+			)).toList());
+
+		// TODO: 역방향 운임
+		stationFares.addAll(stationFareData.stream()
+			.map(data -> StationFare.create(
+				stations.get(data.arrivalStation()),
+				stations.get(data.departureStation()),
+				data.standardFare(),
+				data.firstClassFare()
+			)).toList());
+
+		stationFareRepository.saveAll(stationFares);
+		log.info("{}개 운임 데이터 저장 완료", stationFares.size());
 	}
 }
--- src/main/java/com/sudo/railo/train/infrastructure/excel/StationFareParser.java
+++ src/main/java/com/sudo/railo/train/infrastructure/excel/StationFareParser.java
@@ -1,6 +1,8 @@
 package com.sudo.railo.train.infrastructure.excel;
 
+import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 import org.apache.poi.ss.usermodel.Cell;
@@ -13,9 +15,13 @@ import org.springframework.stereotype.Component;
 import org.springframework.util.ObjectUtils;
 import org.springframework.util.StringUtils;
 
+import com.sudo.railo.train.application.dto.excel.StationFareData;
 import com.sudo.railo.train.application.dto.excel.StationFareHeader;
 import com.sudo.railo.train.application.dto.excel.StationFareHeaderType;
 
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
 @Component
 public class StationFareParser extends ExcelParser {
 
@@ -56,11 +62,46 @@ public class StationFareParser extends ExcelParser {
 		}
 
 		return new StationFareHeader(
-			last,
+			last + 1,
 			headerMap.get(StationFareHeaderType.SECTION),
 			headerMap.get(StationFareHeaderType.STANDARD),
 			headerMap.getOrDefault(StationFareHeaderType.FIRST_CLASS,
 				headerMap.get(StationFareHeaderType.SUPERIOR_CLASS))
 		);
+	}
+
+	public List<StationFareData> getStationFareData(Sheet sheet, StationFareHeader header) {
+		int departureStationIdx = header.section().getColumn();
+		int arrivalStationIdx = header.section().getColumn() + 1;
+		int standardIdx = header.standard().getColumn();
+		int firstClassIdx = header.firstClass().getColumn() + 2;
+
+		List<StationFareData> stationFareData = new ArrayList<>();
+
+		for (int rowNum = header.startRow(); rowNum <= sheet.getLastRowNum(); rowNum++) {
+			try {
+				Row row = sheet.getRow(rowNum);
+
+				// `row`가 `null`이거나, `cell`이 `null`이라면 파싱하지 않는다.
+				if (isEmpty(row, departureStationIdx)) {
+					break;
+				}
+
+				String departureStation = row.getCell(departureStationIdx).getStringCellValue();
+				String arrivalStation = row.getCell(arrivalStationIdx).getStringCellValue();
+				int standardFare = (int)row.getCell(standardIdx).getNumericCellValue();
+				int firstClassFare = (int)row.getCell(firstClassIdx).getNumericCellValue();
+
+				stationFareData.add(new StationFareData(
+					departureStation,
+					arrivalStation,
+					standardFare,
+					firstClassFare
+				));
+			} catch (Exception ex) {
+				log.error("운임표 파싱 중 예외가 발생했습니다", ex);
+			}
+		}
+		return stationFareData;
 	}
 }
