6423cd07fe55166379b72cf8ad98c8ac6803332e
Jimin730 <dnddnd120@naver.com>
2025-07-07 14:12:05 +0900

    feat: Swagger 문서화 (goorm-sudo/raillo-backend#75)

--- src/main/java/com/sudo/railo/member/application/dto/request/SendCodeResponse.java
+++
@@ -1,6 +0,0 @@
-package com.sudo.railo.member.application.dto.request;
-
-public record SendCodeResponse(
-	String email
-) {
-}
--- src/main/java/com/sudo/railo/member/docs/AuthControllerDocs.java
+++ src/main/java/com/sudo/railo/member/docs/AuthControllerDocs.java
@@ -1,12 +1,18 @@
 package com.sudo.railo.member.docs;
 
+import org.springframework.http.ResponseEntity;
+
 import com.sudo.railo.global.exception.error.ErrorResponse;
 import com.sudo.railo.global.success.SuccessResponse;
 import com.sudo.railo.member.application.dto.request.MemberNoLoginRequest;
+import com.sudo.railo.member.application.dto.request.SendCodeRequest;
 import com.sudo.railo.member.application.dto.request.SignUpRequest;
+import com.sudo.railo.member.application.dto.request.VerifyCodeRequest;
 import com.sudo.railo.member.application.dto.response.ReissueTokenResponse;
+import com.sudo.railo.member.application.dto.response.SendCodeResponse;
 import com.sudo.railo.member.application.dto.response.SignUpResponse;
 import com.sudo.railo.member.application.dto.response.TokenResponse;
+import com.sudo.railo.member.application.dto.response.VerifyCodeResponse;
 
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.media.Content;
@@ -50,4 +56,26 @@ public interface AuthControllerDocs {
 		@ApiResponse(responseCode = "401", description = "유효하지 않은 토큰입니다.", content = @Content(schema = @Schema(implementation = ErrorResponse.class)))
 	})
 	SuccessResponse<ReissueTokenResponse> reissue(HttpServletRequest request);
+
+	@Operation(method = "POST", summary = "인증되지 않은 사용자용 이메일 인증코드 전송 요청", description = "회원번호 찾기, 비밀번호 찾기 등 로그인 할 수 없는 상황에서 사용되는 이메일 인증 요청입니다.")
+	@ApiResponses(value = {
+		@ApiResponse(responseCode = "200", description = "이메일 인증 코드 전송을 성공했습니다."),
+		@ApiResponse(responseCode = "400", description = "요청 본문이 유효하지 않습니다.", content = @Content(schema = @Schema(implementation = ErrorResponse.class)))
+	})
+	SuccessResponse<SendCodeResponse> sendAuthCode(SendCodeRequest request);
+
+	@Operation(method = "POST", summary = "인증된 사용자용 이메일 인증코드 전송 요청", description = "이메일 변경, 휴대폰 번호 변경 등 로그인 되어 있는 상태에서 사용되는 이메일 인증 요청입니다.",
+		security = {@SecurityRequirement(name = "bearerAuth")})
+	@ApiResponses(value = {
+		@ApiResponse(responseCode = "200", description = "이메일 인증 코드 전송을 성공했습니다."),
+		@ApiResponse(responseCode = "400", description = "요청 본문이 유효하지 않습니다.", content = @Content(schema = @Schema(implementation = ErrorResponse.class)))
+	})
+	SuccessResponse<?> sendAuthCodeWithMember(String memberNo);
+
+	@Operation(method = "POST", summary = "이메일 인증 코드 검증", description = "인증된 사용자와 인증되지 않은 사용자 모두 이메일 인증 코드 검증 시 사용합니다.")
+	@ApiResponses(value = {
+		@ApiResponse(responseCode = "200", description = "인증 코드 검증에 성공했을 경우 true, 실패했을 경우 false"),
+		@ApiResponse(responseCode = "400", description = "요청 본문이 유효하지 않습니다.", content = @Content(schema = @Schema(implementation = ErrorResponse.class)))
+	})
+	ResponseEntity<VerifyCodeResponse> verifyAuthCode(VerifyCodeRequest request);
 }
