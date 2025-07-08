115d8f155315f635b8040c643cacced98bccd86b
Jimin730 <dnddnd120@naver.com>
2025-07-08 10:03:42 +0900

    refactor: 불필요한 코드 제거 (goorm-sudo/raillo-backend#75)

--- src/main/java/com/sudo/railo/global/security/SecurityConfig.java
+++ src/main/java/com/sudo/railo/global/security/SecurityConfig.java
@@ -61,7 +61,6 @@ public class SecurityConfig {
 			.authorizeHttpRequests(auth -> {
 				auth.requestMatchers("/", "/auth/signup", "/auth/login").permitAll()
 					.requestMatchers(HttpMethod.POST, "/auth/emails/**").permitAll()
-					.requestMatchers("/api/v1/guest/register", "/api/v1/train-schedule/**").permitAll()
 					.requestMatchers("/api/v1/guest/register", "/api/v1/trains/**").permitAll()
 					.requestMatchers("/v3/api-docs/**", "/swagger-ui/**", "/swagger-ui.html").permitAll()
 					.requestMatchers("/actuator/**").permitAll()
