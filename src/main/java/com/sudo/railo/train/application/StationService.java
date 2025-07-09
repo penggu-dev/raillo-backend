e11aa5248cc5f86c43137330a4953fd98fe5cf05
조윤성 <yunsung1206@gmail.com>
2025-07-09 13:41:42 +0900

    feat: 역 저장에 BulkInsert 적용 (goorm-sudo/raillo-backend#83)

--- src/main/java/com/sudo/railo/train/application/StationService.java
+++ src/main/java/com/sudo/railo/train/application/StationService.java
@@ -2,36 +2,47 @@ package com.sudo.railo.train.application;
 
 import java.util.List;
 import java.util.Map;
+import java.util.Set;
+import java.util.function.Function;
 import java.util.stream.Collectors;
 
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import com.sudo.railo.train.domain.Station;
+import com.sudo.railo.train.infrastructure.StationJdbcRepository;
 import com.sudo.railo.train.infrastructure.StationRepository;
 
-import jakarta.transaction.Transactional;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 
+@Slf4j
 @Service
 @RequiredArgsConstructor
 public class StationService {
 
 	private final StationRepository stationRepository;
+	private final StationJdbcRepository stationJdbcRepository;
+
+	@Transactional(readOnly = true)
+	public Map<String, Station> getStationMap() {
+		return stationRepository.findAll().stream()
+			.collect(Collectors.toMap(Station::getStationName, Function.identity()));
+	}
 
 	@Transactional
-	public Map<String, Station> findOrCreateStation(List<String> stationNames) {
-		Map<String, Station> stationMap = stationRepository.findByStationNameIn(stationNames).stream()
-			.collect(Collectors.toMap(Station::getStationName, station -> station));
+	public void createStations(Set<String> stationNames) {
+		Map<String, Station> stationMap = getStationMap();
 
-		List<Station> newStations = stationNames.stream()
+		// 역 생성
+		List<Station> stations = stationNames.stream()
 			.filter(name -> !stationMap.containsKey(name))
 			.map(Station::create)
 			.toList();
 
-		if (!newStations.isEmpty()) {
-			stationRepository.saveAll(newStations);
-			newStations.forEach(station -> stationMap.put(station.getStationName(), station));
+		if (!stations.isEmpty()) {
+			stationJdbcRepository.bulkInsertStations(stations);
+			log.info("{}개의 역 저장 완료", stations.size());
 		}
-		return stationMap;
 	}
 }
---
+++ src/main/java/com/sudo/railo/train/infrastructure/StationJdbcRepositoryImpl.java
@@ -0,0 +1,25 @@
+package com.sudo.railo.train.infrastructure;
+
+import java.util.List;
+
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.stereotype.Repository;
+
+import com.sudo.railo.train.domain.Station;
+
+import lombok.RequiredArgsConstructor;
+
+@Repository
+@RequiredArgsConstructor
+public class StationJdbcRepositoryImpl implements StationJdbcRepository {
+
+	private final JdbcTemplate jdbcTemplate;
+
+	@Override
+	public void bulkInsertStations(List<Station> stations) {
+		String sql = "INSERT INTO station (station_name) VALUES (?)";
+
+		jdbcTemplate.batchUpdate(sql, stations, stations.size(), (ps, station) ->
+			ps.setString(1, station.getStationName()));
+	}
+}
