0cfd06378768b1e043467e6c9e98b1e6145d0b50
chanwon <clw8679@naver.com>
2025-07-17 16:23:26 +0900

    feat: 테스트 전용 커스텀 어노테이션 구현 (goorm-sudo/raillo-backend#154)

---
+++ src/test/java/com/sudo/railo/support/annotation/ServiceTest.java
@@ -0,0 +1,18 @@
+package com.sudo.railo.support.annotation;
+
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
+
+import com.sudo.railo.support.extension.DatabaseCleanupExtension;
+import com.sudo.railo.support.extension.RedisServerExtension;
+
+@ActiveProfiles("test")
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+@Retention(RetentionPolicy.RUNTIME)
+@ExtendWith({RedisServerExtension.class, DatabaseCleanupExtension.class})
+public @interface ServiceTest {
+}
