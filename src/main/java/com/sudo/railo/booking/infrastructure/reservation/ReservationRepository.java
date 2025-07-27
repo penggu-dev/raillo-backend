474565d60c0613b0099fa7d017f9cbc7a4f7e47e
Friox <devfriox@gmail.com>
2025-07-28 02:32:57 +0900

    feat: 만료된 예약을 검색하는 메서드 추가 (Pageable) (goorm-sudo/raillo-backend#165)

--- src/main/java/com/sudo/railo/booking/infrastructure/reservation/ReservationRepository.java
+++ src/main/java/com/sudo/railo/booking/infrastructure/reservation/ReservationRepository.java
@@ -1,13 +1,18 @@
 package com.sudo.railo.booking.infrastructure.reservation;
 
 import java.time.LocalDateTime;
+import java.util.List;
 
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.sudo.railo.booking.domain.Reservation;
 import com.sudo.railo.booking.domain.status.ReservationStatus;
 
 public interface ReservationRepository extends JpaRepository<Reservation, Long> {
-	void deleteAllByExpiresAtBeforeAndReservationStatusNot(LocalDateTime expiresAtBefore,
-		ReservationStatus reservationStatus);
+	List<Reservation> findAllByExpiresAtBeforeAndReservationStatus(
+		LocalDateTime expiresAtBefore,
+		ReservationStatus reservationStatus,
+		Pageable pageable
+	);
 }
