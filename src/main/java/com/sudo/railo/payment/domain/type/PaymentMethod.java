4ae5aefc8a25696b0142d21098b3f888e3e340f9
Kim Yeong Ryeol <freekyr7529@gmail.com>
2025-07-28 16:23:17 +0900

    feat: 결제 수단 Enum 클래스 구현 (goorm-sudo/raillo-backend#171)

---
+++ src/main/java/com/sudo/railo/payment/domain/type/PaymentMethod.java
@@ -0,0 +1,14 @@
+package com.sudo.railo.payment.domain.type;
+
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+
+@Getter
+@RequiredArgsConstructor
+public enum PaymentMethod {
+	CARD("카드", "카드 결제"),
+	TRANSFER("계좌이체", "계좌이체 결제");
+
+	private final String displayName;
+	private final String description;
+}
