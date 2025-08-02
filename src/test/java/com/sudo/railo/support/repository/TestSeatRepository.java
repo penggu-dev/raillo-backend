5326577ed3694a3ad9c3c6b525c8c7e0ae468390
chanwon <clw8679@naver.com>
2025-08-02 16:18:15 +0900

    fix: @Param import문 수정 (goorm-sudo/raillo-backend#195)

--- src/test/java/com/sudo/railo/support/repository/TestSeatRepository.java
+++ src/test/java/com/sudo/railo/support/repository/TestSeatRepository.java
@@ -3,13 +3,11 @@ package com.sudo.railo.support.repository;
 import java.util.List;
 
 import org.springframework.data.domain.Pageable;
-import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 import com.sudo.railo.train.domain.Seat;
 import com.sudo.railo.train.domain.type.CarType;
-
-import io.lettuce.core.dynamic.annotation.Param;
 
 /**
  * 테스트에서만 사용하는 Seat 조회 Repository
@@ -18,7 +16,7 @@ public interface TestSeatRepository extends JpaRepository<Seat, Long> {
 
 	@Query("SELECT s FROM Seat s JOIN FETCH s.trainCar tc JOIN FETCH tc.train t " +
 		"WHERE t.id = :trainId AND tc.carType = :carType")
-	List<Seat> findByTrainIdAndCarTypeWithTrainCarLimited(@Param("trainId") Long trainId, @Param("carType") CarType carType, Pageable pageable);
+	List<Seat> findByTrainIdAndCarTypeWithTrainCarLimited(Long trainId, CarType carType, Pageable pageable);
 
 	@Query("SELECT s FROM Seat s JOIN FETCH s.trainCar tc JOIN FETCH tc.train t WHERE t.id = :trainId")
 	List<Seat> findByTrainIdWithTrainCar(Long trainId);
