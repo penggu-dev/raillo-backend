29bd44e03c23de6a1159447c78eb3f77bd07bd88
Jimin730 <dnddnd120@naver.com>
2025-08-05 12:00:12 +0900

    feat: id값만 받아오도록 쿼리 수정 (goorm-sudo/raillo-backend#190)

--- src/main/java/com/sudo/railo/member/batch/DeleteExpiredMembersJobConfig.java
+++ src/main/java/com/sudo/railo/member/batch/DeleteExpiredMembersJobConfig.java
@@ -1,10 +1,12 @@
 package com.sudo.railo.member.batch;
 
 import java.time.LocalDateTime;
+import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
-import java.util.Objects;
+
+import javax.sql.DataSource;
 
 import org.springframework.batch.core.Job;
 import org.springframework.batch.core.Step;
@@ -12,9 +14,10 @@ import org.springframework.batch.core.job.builder.JobBuilder;
 import org.springframework.batch.core.repository.JobRepository;
 import org.springframework.batch.core.step.builder.StepBuilder;
 import org.springframework.batch.item.ItemWriter;
-import org.springframework.batch.item.database.JpaPagingItemReader;
-import org.springframework.batch.item.database.builder.JpaPagingItemReaderBuilder;
-import org.springframework.batch.item.database.orm.JpaNativeQueryProvider;
+import org.springframework.batch.item.database.JdbcPagingItemReader;
+import org.springframework.batch.item.database.Order;
+import org.springframework.batch.item.database.builder.JdbcPagingItemReaderBuilder;
+import org.springframework.batch.item.database.support.MySqlPagingQueryProvider;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.retry.backoff.ExponentialBackOffPolicy;
@@ -22,10 +25,8 @@ import org.springframework.retry.policy.SimpleRetryPolicy;
 import org.springframework.retry.support.RetryTemplate;
 import org.springframework.transaction.PlatformTransactionManager;
 
-import com.sudo.railo.member.domain.Member;
 import com.sudo.railo.member.infrastructure.MemberRepository;
 
-import jakarta.persistence.EntityManagerFactory;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 
@@ -36,8 +37,8 @@ public class DeleteExpiredMembersJobConfig {
 
 	private final JobRepository jobRepository;
 	private final PlatformTransactionManager transactionManager;
-	private final EntityManagerFactory entityManagerFactory;
 	private final MemberRepository memberRepository;
+	private final DataSource dataSource;
 
 	private static final int CHUNK_SIZE = 100; // 100개씩 처리
 
@@ -51,42 +52,43 @@ public class DeleteExpiredMembersJobConfig {
 	@Bean
 	public Step deleteExpiredMembersStep() {
 		return new StepBuilder("deleteExpiredMembersStep", jobRepository)
-			.<Member, Member>chunk(CHUNK_SIZE, transactionManager)
+			.<Long, Long>chunk(CHUNK_SIZE, transactionManager)
 			.reader(expiredMembersReader())
 			.writer(expiredMembersWriter())
 			.build();
 	}
 
 	@Bean
-	public JpaPagingItemReader<Member> expiredMembersReader() {
+	public JdbcPagingItemReader<Long> expiredMembersReader() {
 
-		JpaNativeQueryProvider<Member> queryProvider = new JpaNativeQueryProvider<>();
-		queryProvider.setEntityClass(Member.class);
+		MySqlPagingQueryProvider queryProvider = new MySqlPagingQueryProvider();
+		queryProvider.setSelectClause("m.id");
+		queryProvider.setFromClause("member m");
+		queryProvider.setWhereClause("m.is_deleted = true AND m.updated_at < :date");
 
-		queryProvider.setSqlQuery("SELECT * FROM member m WHERE m.is_deleted = true AND m.updated_at < :date");
+		Map<String, Order> sortKeys = new HashMap<>();
+		sortKeys.put("id", Order.ASCENDING);
+		queryProvider.setSortKeys(sortKeys);
 
 		// 삭제된지 3년이 지난 회원 조회 파라미터
 		Map<String, Object> parameters = new HashMap<>();
 		parameters.put("date", LocalDateTime.now().minusYears(3));
 
-		// JPA 를 사용하여 페이징 방식으로 데이터 읽어옴
-		return new JpaPagingItemReaderBuilder<Member>()
+		return new JdbcPagingItemReaderBuilder<Long>()
 			.name("expiredMembersReader")
-			.entityManagerFactory(entityManagerFactory)
-			.pageSize(CHUNK_SIZE)
+			.dataSource(dataSource)
 			.queryProvider(queryProvider)
 			.parameterValues(parameters)
+			.rowMapper((rs, rowNum) -> rs.getLong("id"))
+			.pageSize(CHUNK_SIZE)
 			.build();
 	}
 
 	@Bean
-	public ItemWriter<Member> expiredMembersWriter() {
+	public ItemWriter<Long> expiredMembersWriter() {
 		// 영구 삭제 처리
 		return chunk -> {
-			List<Long> memberIds = chunk.getItems().stream()
-				.map(Member::getId)
-				.filter(Objects::nonNull)
-				.toList();
+			List<Long> memberIds = new ArrayList<>(chunk.getItems());
 
 			if (!memberIds.isEmpty()) {
 				log.info("총 {}명의 회원 영구 삭제 처리", memberIds.size());
