504b6d774c4672d8e2abe34e5e708ac1c731837a
Jimin730 <dnddnd120@naver.com>
2025-08-05 11:34:14 +0900

    feat: 재시도 로직 추가 (goorm-sudo/raillo-backend#190)

--- src/main/java/com/sudo/railo/member/batch/DeleteExpiredMembersJobConfig.java
+++ src/main/java/com/sudo/railo/member/batch/DeleteExpiredMembersJobConfig.java
@@ -17,6 +17,9 @@ import org.springframework.batch.item.database.builder.JpaPagingItemReaderBuilde
 import org.springframework.batch.item.database.orm.JpaNativeQueryProvider;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.retry.backoff.ExponentialBackOffPolicy;
+import org.springframework.retry.policy.SimpleRetryPolicy;
+import org.springframework.retry.support.RetryTemplate;
 import org.springframework.transaction.PlatformTransactionManager;
 
 import com.sudo.railo.member.domain.Member;
@@ -88,7 +91,30 @@ public class DeleteExpiredMembersJobConfig {
 			if (!memberIds.isEmpty()) {
 				log.info("총 {}명의 회원 영구 삭제 처리", memberIds.size());
 
-				memberRepository.deleteAllByIdInBatch(memberIds);
+				RetryTemplate retryTemplate = new RetryTemplate();
+				SimpleRetryPolicy retryPolicy = new SimpleRetryPolicy(5); // 최대 5번 재시도
+				retryTemplate.setRetryPolicy(retryPolicy);
+
+				// 백오프 정책 설정
+				ExponentialBackOffPolicy backOffPolicy = new ExponentialBackOffPolicy();
+				backOffPolicy.setInitialInterval(250);
+				backOffPolicy.setMultiplier(2);
+				backOffPolicy.setMaxInterval(1000); //최대 1초 대기
+				retryTemplate.setBackOffPolicy(backOffPolicy);
+
+				try {
+					retryTemplate.execute(context -> {
+						memberRepository.deleteAllByIdInBatch(memberIds);
+						log.info("회원 영구 삭제 성공");
+						return null;
+					}, context -> {
+						throw new RuntimeException("회원 삭제 실패: 최대 재시도 횟수 초과");
+					});
+				} catch (Exception e) {
+					log.error("회원 영구 삭제 처리 중 오류 발생: {}", e.getMessage());
+					throw e;
+				}
+
 			}
 		};
 	}
