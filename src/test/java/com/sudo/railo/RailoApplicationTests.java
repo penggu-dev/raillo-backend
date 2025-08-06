d5085dbcc725b4d166267ff9bb4f3a8393f6de77
Jimin730 <dnddnd120@naver.com>
2025-08-06 23:01:58 +0900

    test: MockMailConfig 제거 (goorm-sudo/raillo-backend#219)

--- src/test/java/com/sudo/railo/RailoApplicationTests.java
+++ src/test/java/com/sudo/railo/RailoApplicationTests.java
@@ -2,14 +2,10 @@ package com.sudo.railo;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.context.annotation.Import;
 import org.springframework.test.context.ActiveProfiles;
-
-import com.sudo.railo.support.MockMailConfig;
 
 @SpringBootTest
 @ActiveProfiles("test")
-@Import(MockMailConfig.class)
 class RailoApplicationTests {
 
 	@Test
