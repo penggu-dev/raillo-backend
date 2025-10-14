f7b72a951330982185c923e56bdd58b18993a4d2
chanwon <clw8679@naver.com>
2025-10-14 23:22:17 +0900

    test: 테스트용 Fixture 코드 추가 (#2)

---
+++ src/test/java/com/sudo/raillo/support/fixture/StationFixture.java
@@ -0,0 +1,25 @@
+package com.sudo.raillo.support.fixture;
+
+import com.sudo.raillo.train.domain.Station;
+import java.lang.reflect.Field;
+
+public class StationFixture {
+
+	public static Station create(String stationName) {
+		try {
+			var constructor = Station.class.getDeclaredConstructor();
+			constructor.setAccessible(true);
+			Station station = constructor.newInstance();
+			setField(station, "stationName", stationName);
+			return station;
+		} catch (Exception e) {
+			throw new RuntimeException("StationFixture 생성 에러", e);
+		}
+	}
+
+	private static void setField(Object target, String fieldName, Object value) throws Exception {
+		Field field = target.getClass().getDeclaredField(fieldName);
+		field.setAccessible(true);
+		field.set(target, value);
+	}
+}
