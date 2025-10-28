eba8bcd201e19f4c122fdb4b2919d7da2c0a861d
Jimin730 <dnddnd120@naver.com>
2025-10-28 17:44:20 +0900

    docs: swagger tag 추가 (#13)

--- src/main/java/com/sudo/raillo/global/config/SwaggerConfig.java
+++ src/main/java/com/sudo/raillo/global/config/SwaggerConfig.java
@@ -101,6 +101,9 @@ public class SwaggerConfig {
 				.name("Reservations")
 				.description("🎫 예매 API - 예매 생성, 조회, 취소, 결제 API"),
 			new Tag()
+				.name("CartReservations")
+				.description("장바구니 API - 예약 등록, 조회 API"),
+			new Tag()
 				.name("Payments")
 				.description("💳 결제 API - 결제 처리, 환불, 결제 내역 API"),
 			new Tag()
