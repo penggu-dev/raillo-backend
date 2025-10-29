523e7bd45ab3c5ad33b956065784028b5b9127e8
chanwon <clw8679@naver.com>
2025-10-29 22:01:29 +0900

    refactor: 충돌 해결

--- src/main/java/com/sudo/raillo/booking/application/service/QrService.java
+++ src/main/java/com/sudo/raillo/booking/application/service/QrService.java
@@ -1,12 +1,18 @@
 package com.sudo.raillo.booking.application.service;
 
-import com.sudo.raillo.booking.domain.Qr;
-import com.sudo.raillo.booking.infrastructure.QrRepository;
-import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+
+import com.sudo.raillo.booking.domain.Qr;
+import com.sudo.raillo.booking.exception.BookingError;
+import com.sudo.raillo.booking.infrastructure.QrRepository;
+import com.sudo.raillo.global.exception.error.BusinessException;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.transaction.annotation.Transactional;
 
 @Service
 @RequiredArgsConstructor
+@Transactional
 public class QrService {
 
 	private final QrRepository qrRepository;
