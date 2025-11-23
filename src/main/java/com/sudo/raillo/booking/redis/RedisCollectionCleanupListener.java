bbaa2e147cb3eca663fd9f6ef432d902b3187c1b
Jimin730 <dnddnd120@naver.com>
2025-11-24 00:15:24 +0900

    불필요한 데이터 삭제 로직 추가 구현

---
+++ src/main/java/com/sudo/raillo/booking/redis/RedisCollectionCleanupListener.java
@@ -0,0 +1,42 @@
+package com.sudo.raillo.booking.redis;
+
+import java.nio.charset.StandardCharsets;
+import java.util.Set;
+
+import org.springframework.data.redis.core.RedisKeyExpiredEvent;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.stereotype.Component;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
+@Component
+@RequiredArgsConstructor
+public class RedisCollectionCleanupListener {
+
+	private final RedisTemplate<String, Object> objectRedisTemplate;
+
+	public void handleRedisKeyExpiredEvent(RedisKeyExpiredEvent<Object> event) {
+		// 1. 만료된 키 가져오기
+		String expiredKey = new String(event.getSource(), StandardCharsets.UTF_8);
+
+		validateReservationKey(expiredKey);
+
+		// 2. 만료된 키와 관련된 모든 인덱스 키 패턴 생성
+		String collectionKeyPattern = expiredKey + ":*";
+
+		// 3. 패턴에 해당하는 모든 관련 키를 Redis에서 조회
+		Set<String> keysToDelete = objectRedisTemplate.keys(collectionKeyPattern);
+
+		if (!keysToDelete.isEmpty()) {
+			// 4. 찾은 컬렉션 키들을 모두 삭제
+			Long deletedCount = objectRedisTemplate.delete(keysToDelete);
+			log.info("TTL 만료. 주 키: [{}]. 관련 컬렉션 키 {}개 삭제 완료.", expiredKey, deletedCount);
+		}
+	}
+
+	private void validateReservationKey(String expiredKey) {
+		if (!expiredKey.startsWith("reservation:") && !expiredKey.startsWith("seat-reservation:")) {return;}
+	}
+}
--- src/main/java/com/sudo/raillo/global/config/RedisConfig.java
+++ src/main/java/com/sudo/raillo/global/config/RedisConfig.java
@@ -6,6 +6,7 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.data.redis.connection.RedisConnectionFactory;
 import org.springframework.data.redis.connection.RedisStandaloneConfiguration;
 import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
+import org.springframework.data.redis.core.RedisKeyValueAdapter;
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.data.redis.repository.configuration.EnableRedisRepositories;
 import org.springframework.data.redis.serializer.GenericJackson2JsonRedisSerializer;
@@ -18,7 +19,7 @@ import lombok.extern.slf4j.Slf4j;
 
 @Slf4j
 @Configuration
-@EnableRedisRepositories
+@EnableRedisRepositories(enableKeyspaceEvents = RedisKeyValueAdapter.EnableKeyspaceEvents.ON_STARTUP)
 public class RedisConfig {
 
 	@Value("${spring.data.redis.host}")
