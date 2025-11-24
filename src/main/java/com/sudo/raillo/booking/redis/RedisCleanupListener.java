ed8a6a63f571cc22a44a92212f1bf80cb4348371
Jimin730 <dnddnd120@naver.com>
2025-11-24 15:51:59 +0900

    네이밍 변경

--- src/main/java/com/sudo/raillo/booking/redis/RedisCleanupListener.java
+++ src/main/java/com/sudo/raillo/booking/redis/RedisCleanupListener.java
@@ -13,7 +13,7 @@ import lombok.extern.slf4j.Slf4j;
 @Slf4j
 @Component
 @RequiredArgsConstructor
-public class RedisCollectionCleanupListener {
+public class RedisCleanupListener {
 
 	private final RedisTemplate<String, Object> objectRedisTemplate;
 
