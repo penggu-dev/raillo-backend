d6488f6966a5369c9e90542a422403f293fa822d
Jimin730 <dnddnd120@naver.com>
2025-12-05 17:36:48 +0900

    feat: PendingBooking Redis 키 생성 로직 구현(#54)

--- src/main/java/com/sudo/raillo/global/redis/RedisKeyGenerator.java
+++ src/main/java/com/sudo/raillo/global/redis/RedisKeyGenerator.java
@@ -8,11 +8,16 @@ public class RedisKeyGenerator {
 	/**
 	 * Redis 키 Prefix
 	 * */
+	// auth
 	private static final String MEMBER_NO_KEY_PREFIX = "member:email:{email}:no";
 	private static final String UPDATE_EMAIL_KEY_PREFIX = "member:email:{email}:update";
 	private static final String EMAIL_AUTH_CODE_KEY_PREFIX = "auth:email:{email}";
 	private static final String REFRESH_TOKEN_KEY_PREFIX = "auth:no:{memberNo}:refreshToken";
 	private static final String LOGOUT_KEY_PREFIX = "auth:accessToken:{token}:logout";
+
+	// booking
+	private static final String PENDING_BOOKING_KEY_PREFIX = "booking:pendingBooking:{pendingBookingId}";
+	private static final String PENDING_BOOKING_MEMBER_KEY_PREFIX = "booking:member:{memberNo}:pendingBooking:%s";
 
 	public String generateMemberNoKey(String email) {
 		return MEMBER_NO_KEY_PREFIX.replace("{email}", email);
@@ -34,4 +39,16 @@ public class RedisKeyGenerator {
 		return LOGOUT_KEY_PREFIX.replace("{token}", accessToken);
 	}
 
+	public String generatePendingBookingKey(String pendingBookingId) {
+		return PENDING_BOOKING_KEY_PREFIX.replace("{pendingBookingId}", pendingBookingId);
+	}
+
+	public String generatePendingBookingMemberKey(String memberNo, String pendingBookingId) {
+		return PENDING_BOOKING_MEMBER_KEY_PREFIX.replace("{memberNo}", memberNo)
+			.replace("%s", pendingBookingId);
+	}
+
+	public String generatePendingBookingMemberKeyPattern(String memberNo) {
+		return PENDING_BOOKING_MEMBER_KEY_PREFIX.replace("{memberNo}", memberNo);
+	}
 }
