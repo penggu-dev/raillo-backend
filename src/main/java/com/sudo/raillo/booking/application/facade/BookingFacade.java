ba1d2009183c9378c24f85ddf1c66653f630ce8e
Jimin730 <dnddnd120@naver.com>
2025-12-16 23:38:30 +0900

    feat: PendingBooking 생성 로직 구현 (#63)

--- src/main/java/com/sudo/raillo/booking/application/facade/BookingFacade.java
+++ src/main/java/com/sudo/raillo/booking/application/facade/BookingFacade.java
@@ -1,19 +1,21 @@
 package com.sudo.raillo.booking.application.facade;
 
-import com.sudo.raillo.booking.application.dto.request.BookingCreateRequest;
-import com.sudo.raillo.booking.application.dto.response.BookingCreateResponse;
+import com.sudo.raillo.booking.application.dto.request.PendingBookingCreateRequest;
+import com.sudo.raillo.booking.application.dto.response.PendingBookingCreateResponse;
 import com.sudo.raillo.booking.application.service.BookingService;
+import com.sudo.raillo.booking.application.service.FareCalculationService;
+import com.sudo.raillo.booking.application.service.PendingBookingService;
 import com.sudo.raillo.booking.application.service.SeatBookingService;
 import com.sudo.raillo.booking.application.service.TicketService;
-import com.sudo.raillo.booking.application.validator.BookingValidator;
 import com.sudo.raillo.booking.domain.Booking;
-import com.sudo.raillo.booking.domain.type.PassengerSummary;
+import com.sudo.raillo.booking.domain.PendingBooking;
 import com.sudo.raillo.member.domain.Member;
 import com.sudo.raillo.train.domain.type.CarType;
-import java.util.ArrayList;
-import java.util.Comparator;
-import java.util.List;
+
+import java.math.BigDecimal;
+
 import lombok.RequiredArgsConstructor;
+
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -23,27 +25,25 @@ import org.springframework.transaction.annotation.Transactional;
 public class BookingFacade {
 
 	private final BookingService bookingService;
+	private final PendingBookingService pendingBookingService;
 	private final SeatBookingService seatBookingService;
 	private final TicketService ticketService;
-	private final BookingValidator bookingValidator;
+	private final FareCalculationService fareCalculationService;
 
-	public BookingCreateResponse createBooking(BookingCreateRequest request, String memberNo) {
-		// TODO: 요청 파라미터를 여기서 모두 검증할지, 각 서비스에서 검증할지 결정 필요
-		CarType carType = bookingService.findCarType(request.seatIds());
+	public PendingBookingCreateResponse createBooking(PendingBookingCreateRequest request, String memberNo) {
+		CarType carType = pendingBookingService.findCarType(request.seatIds());
+		BigDecimal totalFare = fareCalculationService.calculateFare(
+			request.departureStationId(),
+			request.arrivalStationId(),
+			request.passengerTypes(),
+			carType
+		);
 
-		Booking booking = bookingService.createBooking(request, memberNo);
-		bookingValidator.validateStopSequence(booking);
+		// TODO: 좌석락 로직 필요
 
-		// 승객 정보, 좌석 정보 정렬 (승객 정보는 PassengerType에 정의한 순서대로, 좌석 정보는 오름차순)
-		List<PassengerSummary> passengers = new ArrayList<>(request.passengers());
-		passengers.sort(Comparator.comparingInt(ps -> ps.getPassengerType().ordinal()));
-		List<Long> seatIds = new ArrayList<>(request.seatIds());
-		seatIds.sort(Comparator.naturalOrder());
+		PendingBooking pendingBooking = pendingBookingService.createPendingBooking(request, memberNo, totalFare);
 
-		bookingValidator.validatePassengerSeatCount(passengers, seatIds);
-		List<Long> seatBookingIds = seatBookingService.createSeatBookings(booking, passengers, seatIds);
-
-		return new BookingCreateResponse(booking.getId(), seatBookingIds);
+		return new PendingBookingCreateResponse(pendingBooking.getId());
 	}
 
 	public void cancelBooking(Booking booking) {
