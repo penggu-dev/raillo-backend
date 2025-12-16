c3c75b403642c3b7b0d33a57193639d8805c435b
Ogu1208 <kdasunb6@gmail.com>
2025-12-17 00:21:08 +0900

    refactor: 결제 시 예약 개수 제한 변경 (10 -> 5) (#57)
    refactor: 결제 시 예약 개수 제한 변경 (10 -> 5) (#57)

--- src/main/java/com/sudo/raillo/payment/application/dto/request/PaymentPrepareRequest.java
+++ src/main/java/com/sudo/raillo/payment/application/dto/request/PaymentPrepareRequest.java
@@ -12,7 +12,7 @@ public record PaymentPrepareRequest(
 
 	@Schema(defaultValue = "임시 예약 ID 목록 (Redis에 저장된 PendingBooking의 ID - UUID")
 	@NotEmpty(message = "임시 예약 ID 목록은 필수입니다")
-	@Size(min = 1, max = 10, message = "한 번에 최대 10개의 예약까지 결제 가능합니다")
-	List<@NotBlank(message = "임시 예약 ID는 공백일 수 없습니다") String> pendingBookingIds // TODO : max를 정할지 말지 고민..
+	@Size(min = 1, max = 5, message = "한 번에 최대 5개의 예약까지 결제 가능합니다")
+	List<@NotBlank(message = "임시 예약 ID는 공백일 수 없습니다") String> pendingBookingIds
 ) {
 }
