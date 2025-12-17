05c071974d4ceb40feaa76866ebb3fcc5819ce92
Jimin730 <dnddnd120@naver.com>
2025-12-17 23:56:23 +0900

    feat: 주문 및 좌석 관련 에러코드 추가 (#64)

--- src/main/java/com/sudo/raillo/order/exception/OrderError.java
+++ src/main/java/com/sudo/raillo/order/exception/OrderError.java
@@ -9,11 +9,14 @@ import org.springframework.http.HttpStatus;
 @RequiredArgsConstructor
 public enum OrderError implements ErrorCode {
 	NOT_PENDING("주문 대기 상태가 아닙니다", HttpStatus.BAD_REQUEST, "O_001"),
-	NOT_ORDERED("주문 상태가 아닙니다", HttpStatus.BAD_REQUEST, "O_002"),
+	NOT_ORDERED("결제 완료된 주문 상태가 아닙니다", HttpStatus.BAD_REQUEST, "O_002"),
 	INVALID_TOTAL_AMOUNT("주문 총 금액은 0보다 크거나 같아야 합니다", HttpStatus.BAD_REQUEST, "O_003"),
 	ORDER_NOT_FOUND("주문 정보를 찾을 수 없습니다.", HttpStatus.BAD_REQUEST, "O_004"),
 	ORDER_ACCESS_DENIED("주문에 대한 접근 권한이 없습니다.", HttpStatus.FORBIDDEN, "O_005"),
-	EMPTY_PENDING_BOOKINGS("주문할 예약 정보가 없습니다.", HttpStatus.BAD_REQUEST, "O_006");
+	EMPTY_PENDING_BOOKINGS("주문할 예약 정보가 없습니다.", HttpStatus.BAD_REQUEST, "O_006"),
+	ORDER_BOOKING_NOT_FOUND("주문 예약 정보를 찾을 수 없습니다.", HttpStatus.BAD_REQUEST, "O_007"),
+	ORDER_SEAT_BOOKING_NOT_FOUND("주문 좌석 예약 정보를 찾을 수 없습니다.", HttpStatus.BAD_REQUEST, "O_008"),
+	ORDER_IS_EXPIRED("만료된 주문입니다.", HttpStatus.GONE, "O_009");
 
 	private final String message;
 	private final HttpStatus status;
