4397e52df9b3f0f101a455df88ffe62883717fe1
chanwon <clw8679@naver.com>
2025-12-25 02:42:58 +0900

    test: fixture 구현 (#70)

--- src/test/java/com/sudo/raillo/support/fixture/PaymentFixture.java
+++ src/test/java/com/sudo/raillo/support/fixture/PaymentFixture.java
@@ -2,85 +2,48 @@ package com.sudo.raillo.support.fixture;
 
 import java.math.BigDecimal;
 
-import com.sudo.raillo.payment.application.dto.request.PaymentProcessAccountRequest;
-import com.sudo.raillo.payment.application.dto.request.PaymentProcessCardRequest;
+import com.sudo.raillo.member.domain.Member;
+import com.sudo.raillo.order.domain.Order;
+import com.sudo.raillo.payment.domain.Payment;
 
-import lombok.Getter;
+import lombok.AccessLevel;
+import lombok.NoArgsConstructor;
 
-@Getter
-public enum PaymentFixture {
+@NoArgsConstructor(access = AccessLevel.PRIVATE)
+public class PaymentFixture {
 
-	CARD_PAYMENT("1234-5678-9012-3456", "1225", "000505", 0, 12),
+	private Member member;
+	private Order order;
+	private BigDecimal amount = BigDecimal.valueOf(10000);
 
-	ACCOUNT_PAYMENT("088", "123456789012", "홍길동", "000505", "75");
-
-	// 카드 결제 관련 필드
-	private final String cardNumber;
-	private final String validThru;
-	private final String rrn;
-	private final Integer installmentMonths;
-	private final Integer cardPassword;
-
-	// 계좌이체 관련 필드
-	private final String bankCode;
-	private final String accountNumber;
-	private final String accountHolderName;
-	private final String identificationNumber;
-	private final String accountPassword;
-
-	PaymentFixture(String cardNumber, String validThru, String rrn, Integer installmentMonths,
-		Integer cardPassword) {
-		this.cardNumber = cardNumber;
-		this.validThru = validThru;
-		this.rrn = rrn;
-		this.installmentMonths = installmentMonths;
-		this.cardPassword = cardPassword;
-		// 계좌이체 필드는 null로 초기화
-		this.bankCode = null;
-		this.accountNumber = null;
-		this.accountHolderName = null;
-		this.identificationNumber = null;
-		this.accountPassword = null;
+	public static Payment create(Member member, Order order) {
+		return builder()
+			.withMember(member)
+			.withOrder(order)
+			.build();
 	}
 
-	PaymentFixture(String bankCode, String accountNumber, String accountHolderName,
-		String identificationNumber, String accountPassword) {
-		this.bankCode = bankCode;
-		this.accountNumber = accountNumber;
-		this.accountHolderName = accountHolderName;
-		this.identificationNumber = identificationNumber;
-		this.accountPassword = accountPassword;
-		// 카드 결제 필드는 null로 초기화
-		this.cardNumber = null;
-		this.validThru = null;
-		this.rrn = null;
-		this.installmentMonths = null;
-		this.cardPassword = null;
+	// builder method
+	public static PaymentFixture builder() {
+		return new PaymentFixture();
 	}
 
-	public static PaymentProcessCardRequest createCardPaymentRequest(Long bookingId,
-		BigDecimal amount) {
-		PaymentProcessCardRequest request = new PaymentProcessCardRequest();
-		request.setBookingId(bookingId);
-		request.setAmount(amount);
-		request.setCardNumber(CARD_PAYMENT.cardNumber);
-		request.setValidThru(CARD_PAYMENT.validThru);
-		request.setRrn(CARD_PAYMENT.rrn);
-		request.setInstallmentMonths(CARD_PAYMENT.installmentMonths);
-		request.setCardPassword(CARD_PAYMENT.cardPassword);
-		return request;
+	public Payment build() {
+		return Payment.create(member, order, amount);
 	}
 
-	public static PaymentProcessAccountRequest createAccountPaymentRequest(Long bookingId,
-		BigDecimal amount) {
-		PaymentProcessAccountRequest request = new PaymentProcessAccountRequest();
-		request.setBookingId(bookingId);
-		request.setAmount(amount);
-		request.setBankCode(ACCOUNT_PAYMENT.bankCode);
-		request.setAccountNumber(ACCOUNT_PAYMENT.accountNumber);
-		request.setAccountHolderName(ACCOUNT_PAYMENT.accountHolderName);
-		request.setIdentificationNumber(ACCOUNT_PAYMENT.identificationNumber);
-		request.setAccountPassword(ACCOUNT_PAYMENT.accountPassword);
-		return request;
+	public PaymentFixture withMember(Member member) {
+		this.member = member;
+		return this;
+	}
+
+	public PaymentFixture withOrder(Order order) {
+		this.order = order;
+		return this;
+	}
+
+	public PaymentFixture withAmount(BigDecimal amount) {
+		this.amount = amount;
+		return this;
 	}
 }
---
+++ src/test/java/com/sudo/raillo/support/fixture/TicketFixture.java
@@ -0,0 +1,68 @@
+package com.sudo.raillo.support.fixture;
+
+import com.sudo.raillo.booking.domain.Booking;
+import com.sudo.raillo.booking.domain.Ticket;
+import com.sudo.raillo.booking.domain.status.TicketStatus;
+import com.sudo.raillo.booking.domain.type.PassengerType;
+import com.sudo.raillo.train.domain.Seat;
+
+import com.sudo.raillo.train.domain.type.SeatType;
+import lombok.AccessLevel;
+import lombok.NoArgsConstructor;
+
+@NoArgsConstructor(access = AccessLevel.PRIVATE)
+public class TicketFixture {
+
+	private Booking booking;
+	private Seat seat = SeatFixture.create(null, 1, "A", SeatType.WINDOW, "N", "Y");
+	private TicketStatus ticketStatus = TicketStatus.ISSUED;
+	private PassengerType passengerType = PassengerType.ADULT;
+	private String vendorCode = "01";
+	private String purchaseDate = "0101";
+	private String purchaseSeq = "10001";
+	private String purchaseUid = "01";
+
+	public static Ticket create(Booking booking) {
+		return builder()
+			.withBooking(booking)
+			.build();
+	}
+
+	// builder method
+	public static TicketFixture builder() {
+		return new TicketFixture();
+	}
+
+	public Ticket build() {
+		return Ticket.builder()
+			.booking(booking)
+			.seat(seat)
+			.ticketStatus(ticketStatus)
+			.passengerType(passengerType)
+			.vendorCode(vendorCode)
+			.purchaseDate(purchaseDate)
+			.purchaseSeq(purchaseSeq)
+			.purchaseUid(purchaseUid)
+			.build();
+	}
+
+	public TicketFixture withBooking(Booking booking) {
+		this.booking = booking;
+		return this;
+	}
+
+	public TicketFixture withSeat(Seat seat) {
+		this.seat = seat;
+		return this;
+	}
+
+	public TicketFixture withTicketStatus(TicketStatus ticketStatus) {
+		this.ticketStatus = ticketStatus;
+		return this;
+	}
+
+	public TicketFixture withPassengerType(PassengerType passengerType) {
+		this.passengerType = passengerType;
+		return this;
+	}
+}
