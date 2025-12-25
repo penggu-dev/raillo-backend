6921de7af7f5081a969c3f6d644822a6c9a2c518
Jimin730 <dnddnd120@naver.com>
2025-12-25 12:52:46 +0900

    refactor: BookingControllerDoc Swagger 설명 수정 (#71)

--- src/main/java/com/sudo/raillo/booking/docs/BookingControllerDoc.java
+++ src/main/java/com/sudo/raillo/booking/docs/BookingControllerDoc.java
@@ -20,30 +20,28 @@ import io.swagger.v3.oas.annotations.tags.Tag;
 @Tag(name = "Bookings")
 public interface BookingControllerDoc {
 
-	@Operation(method = "DELETE", summary = "예약 삭제", description = "예약 ID를 받아 해당 예약을 삭제합니다.")
+	@Operation(method = "DELETE", summary = "확정 예약 삭제", description = "예약 ID를 받아 해당 확정 예약을 삭제합니다.")
 	@ApiResponses(value = {
-		@ApiResponse(responseCode = "204", description = "예약이 성공적으로 삭제되었습니다.")
+		@ApiResponse(responseCode = "204", description = "확정 예약이 성공적으로 삭제되었습니다.")
 	})
 	SuccessResponse<?> deleteBooking(BookingDeleteRequest request);
 
-	@Operation(method = "GET", summary = "예약 상세 조회", description = "예약 ID를 받아 해당 예약의 상세 정보를 조회합니다.", security = {
+	@Operation(method = "GET", summary = "확정 예약 상세 조회", description = "예약 ID를 받아 해당 확정 예약의 상세 정보를 조회합니다.", security = {
 		@SecurityRequirement(name = "bearerAuth")})
 	@ApiResponses(value = {
-		@ApiResponse(responseCode = "200", description = "예약 상세 정보 조회에 성공했습니다."),
+		@ApiResponse(responseCode = "200", description = "확정 예약 상세 정보 조회에 성공했습니다."),
 		@ApiResponse(responseCode = "404",
-			description = "예약 상세 조회에 실패하였습니다:\n"
+			description = "확정 예약 상세 조회에 실패하였습니다:\n"
 				+ "- 사용자를 찾을 수 없음\n"
-				+ "- 예약 정보를 찾을 수 없음\n",
-			content = @Content(mediaType = "application/json", schema = @Schema(implementation = ErrorResponse.class))),
-		@ApiResponse(responseCode = "410", description = "예약이 만료되었습니다.",
+				+ "- 확정 예약 정보를 찾을 수 없음\n",
 			content = @Content(mediaType = "application/json", schema = @Schema(implementation = ErrorResponse.class)))
 	})
 	SuccessResponse<BookingDetail> getBooking(Long bookingId, UserDetails userDetails);
 
-	@Operation(method = "GET", summary = "예약 목록 조회", description = "인증된 사용자의 모든 예약 목록을 조회합니다.", security = {
+	@Operation(method = "GET", summary = "확정 예약 목록 조회", description = "인증된 사용자의 모든 확정 예약 목록을 조회합니다.", security = {
 		@SecurityRequirement(name = "bearerAuth")})
 	@ApiResponses(value = {
-		@ApiResponse(responseCode = "200", description = "예약 목록 조회에 성공했습니다."),
+		@ApiResponse(responseCode = "200", description = "확정 예약 목록 조회에 성공했습니다."),
 		@ApiResponse(responseCode = "404", description = "사용자를 찾을 수 없습니다.",
 			content = @Content(mediaType = "application/json", schema = @Schema(implementation = ErrorResponse.class)))
 	})
