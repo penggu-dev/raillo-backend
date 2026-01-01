c1fde5d504c89ce4f6efa0a7adfabac341da2f73
Jimin730 <dnddnd120@naver.com>
2026-01-01 21:58:20 +0900

    feature: 배치 조회 쿼리 추가 (#75)

--- src/main/java/com/sudo/raillo/train/infrastructure/ScheduleStopRepository.java
+++ src/main/java/com/sudo/raillo/train/infrastructure/ScheduleStopRepository.java
@@ -1,8 +1,12 @@
 package com.sudo.raillo.train.infrastructure;
 
+import java.util.Collection;
+import java.util.List;
 import java.util.Optional;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import com.sudo.raillo.train.domain.ScheduleStop;
@@ -11,4 +15,7 @@ import com.sudo.raillo.train.domain.ScheduleStop;
 public interface ScheduleStopRepository extends JpaRepository<ScheduleStop, Long> {
 
 	Optional<ScheduleStop> findByTrainScheduleIdAndStationId(Long trainScheduleId, Long id);
+
+	@Query("SELECT ss FROM ScheduleStop ss JOIN FETCH ss.station WHERE ss.id IN :stopIds")
+	List<ScheduleStop> findAllByIdWithStation(@Param("stopIds") Collection<Long> stopIds);
 }
