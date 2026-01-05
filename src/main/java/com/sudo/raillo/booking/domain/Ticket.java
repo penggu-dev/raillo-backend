44c7b8fa48af3a5afa716a5b102dfa7f85e594ac
Ogu1208 <kdasunb6@gmail.com>
2026-01-06 00:29:19 +0900

    feat: Ticket 엔티티에 승차권번호 추가, 사용하지 않는 필드 정리 (#95)

--- src/main/java/com/sudo/raillo/booking/domain/Ticket.java
+++ src/main/java/com/sudo/raillo/booking/domain/Ticket.java
@@ -60,16 +60,6 @@ public class Ticket extends BaseEntity {
 	@Comment("승객 유형")
 	private PassengerType passengerType;
 
-	@Comment("결제 위치 번호 (예: 온라인 (01), ~~역(02...))")
-	private String vendorCode;
-
-	@Comment("결제 날짜 (MMdd)")
-	private String purchaseDate;
-
-	@Comment("승차권 결제 순번 (10000~)")
-	private String purchaseSeq;
-
-	@Comment("결제 고유번호 (숫자 2자리)")
-	private String purchaseUid;
-
+	@Comment("승차권 번호")
+	private String ticketNumber;
 }
