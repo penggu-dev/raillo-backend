ce8b9ed8800eef3712ff5657f9b735c7fb918c86
Ogu1208 <kdasunb6@gmail.com>
2026-01-24 20:29:19 +0900

    refactor: ThreadLocalRandom 도입으로 ID 생성 시의 동시성 병목 제거 (#129)

--- src/main/java/com/sudo/raillo/booking/util/PendingBookingIdGenerator.java
+++ src/main/java/com/sudo/raillo/booking/util/PendingBookingIdGenerator.java
@@ -1,8 +1,8 @@
 package com.sudo.raillo.booking.util;
 
-import java.security.SecureRandom;
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
+import java.util.concurrent.ThreadLocalRandom;
 
 import org.springframework.stereotype.Component;
 
@@ -13,7 +13,6 @@ public class PendingBookingIdGenerator {
 	private static final DateTimeFormatter TIMESTAMP_FORMATTER = DateTimeFormatter.ofPattern("yyyyMMddHHmmss");
 	private static final String CHARACTERS = "0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZ";
 	private static final int RANDOM_LENGTH = 6;
-	private static final SecureRandom RANDOM = new SecureRandom();
 
 	/**
 	 * PendingBookingId 생성
@@ -25,8 +24,9 @@ public class PendingBookingIdGenerator {
 		sb.append(PREFIX);
 		sb.append(LocalDateTime.now().format(TIMESTAMP_FORMATTER));
 
+		ThreadLocalRandom random = ThreadLocalRandom.current();
 		for (int i = 0; i < RANDOM_LENGTH; i++) {
-			int index = RANDOM.nextInt(CHARACTERS.length());
+			int index = random.nextInt(CHARACTERS.length());
 			sb.append(CHARACTERS.charAt(index));
 		}
 
