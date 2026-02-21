647c5e40cdfc805584bb02d156754f285ba068fc
chanwonlee <clw8679@naver.com>
2026-02-21 13:29:33 +0900

    refactor: 미사용 findIdsByScheduleIdAndCarType 메서드 제거 (#144)

--- src/main/java/com/sudo/raillo/train/infrastructure/TrainCarRepository.java
+++ src/main/java/com/sudo/raillo/train/infrastructure/TrainCarRepository.java
@@ -1,22 +1,14 @@
 package com.sudo.raillo.train.infrastructure;
 
-import java.util.Collection;
-import java.util.List;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
-
 import com.sudo.raillo.train.domain.Train;
 import com.sudo.raillo.train.domain.TrainCar;
-import com.sudo.raillo.train.domain.type.CarType;
+import java.util.Collection;
+import java.util.List;
+import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface TrainCarRepository extends JpaRepository<TrainCar, Long> {
 
 	List<TrainCar> findByTrainIn(Collection<Train> trains);
 
 	List<TrainCar> findAllByTrainId(Long trainId);
-
-	@Query("SELECT tc.id FROM TrainSchedule ts JOIN ts.train t JOIN TrainCar tc ON tc.train.id = t.id "
-		+ "WHERE ts.id = :trainScheduleId AND tc.carType = :carType")
-	List<Long> findIdsByScheduleIdAndCarType(Long trainScheduleId, CarType carType);
 }
