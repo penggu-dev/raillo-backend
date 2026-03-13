a28834dd2afe0653e714eaf2b7f6b8773ccefebc
Jimin730 <dnddnd120@naver.com>
2026-03-13 17:43:33 +0900

    feat: 예약 생성 및 좌석 hold timer 메트릭 Aspect 추가 (#206)

--- src/main/java/com/sudo/raillo/booking/application/metrics/BookingMetricsAspect.java
+++ src/main/java/com/sudo/raillo/booking/application/metrics/BookingMetricsAspect.java
@@ -1,13 +1,17 @@
 package com.sudo.raillo.booking.application.metrics;
 
+import org.aspectj.lang.ProceedingJoinPoint;
 import org.aspectj.lang.annotation.AfterReturning;
 import org.aspectj.lang.annotation.AfterThrowing;
+import org.aspectj.lang.annotation.Around;
 import org.aspectj.lang.annotation.Aspect;
 import org.springframework.stereotype.Component;
 
 import com.sudo.raillo.booking.exception.BookingError;
 import com.sudo.raillo.global.exception.error.BusinessException;
 
+import io.micrometer.core.instrument.Timer;
+import io.micrometer.core.instrument.Timer.Sample;
 import lombok.RequiredArgsConstructor;
 
 @Aspect
@@ -30,6 +34,25 @@ public class BookingMetricsAspect {
 			bookingMetrics.incrementSeatConflictHold();
 		} else if (e.getErrorCode() == BookingError.SEAT_CONFLICT_WITH_SOLD) {
 			bookingMetrics.incrementSeatConflictSold();
+		}
+	}
+
+	@Around("execution(* com.sudo.raillo.booking.application.facade.PendingBookingFacade.createPendingBooking(..))")
+	public Object timePendingBookingCreation(ProceedingJoinPoint joinPoint) throws Throwable {
+		return recordTime(joinPoint, bookingMetrics.getPendingBookingCreatedTimer());
+	}
+
+	@Around("execution(* com.sudo.raillo.booking.application.service.SeatHoldService.holdSeats(..))")
+	public Object timeSeatHold(ProceedingJoinPoint joinPoint) throws Throwable {
+		return recordTime(joinPoint, bookingMetrics.getSeatHoldTimer());
+	}
+
+	private Object recordTime(ProceedingJoinPoint joinPoint, Timer timer) throws Throwable {
+		Sample sample = Timer.start();
+		try {
+			return joinPoint.proceed();
+		} finally {
+			sample.stop(timer);
 		}
 	}
 }
