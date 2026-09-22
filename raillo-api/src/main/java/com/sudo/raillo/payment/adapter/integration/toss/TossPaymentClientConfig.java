1f2ac2453b7cc2f0aa7ced38a3dabef8205c374a
Ogu1208 <kdasunb6@gmail.com>
2026-09-22 13:04:26 +0900

    refactor: TossPaymentClientConfig를 Apache HttpClient 5로 재작성 (#289)

--- raillo-api/src/main/java/com/sudo/raillo/payment/adapter/integration/toss/TossPaymentClientConfig.java
+++ raillo-api/src/main/java/com/sudo/raillo/payment/adapter/integration/toss/TossPaymentClientConfig.java
@@ -2,16 +2,24 @@ package com.sudo.raillo.payment.adapter.integration.toss;
 
 import java.nio.charset.StandardCharsets;
 import java.util.Base64;
-
-import org.springframework.boot.context.properties.EnableConfigurationProperties;
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
 import java.util.LinkedHashMap;
 import java.util.List;
 import java.util.Map;
 
+import org.apache.hc.client5.http.config.ConnectionConfig;
+import org.apache.hc.client5.http.config.RequestConfig;
+import org.apache.hc.client5.http.impl.classic.CloseableHttpClient;
+import org.apache.hc.client5.http.impl.classic.HttpClients;
+import org.apache.hc.client5.http.impl.io.PoolingHttpClientConnectionManager;
+import org.apache.hc.client5.http.impl.io.PoolingHttpClientConnectionManagerBuilder;
+import org.apache.hc.core5.util.TimeValue;
+import org.apache.hc.core5.util.Timeout;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.MediaType;
+import org.springframework.http.client.HttpComponentsClientHttpRequestFactory;
 import org.springframework.web.client.RestClient;
 
 import lombok.extern.slf4j.Slf4j;
@@ -21,12 +29,50 @@ import lombok.extern.slf4j.Slf4j;
 @EnableConfigurationProperties(TossPaymentProperties.class)
 public class TossPaymentClientConfig {
 
+	@Bean(destroyMethod = "close")
+	public PoolingHttpClientConnectionManager tossHttpConnectionManager(TossPaymentProperties properties) {
+		return PoolingHttpClientConnectionManagerBuilder.create()
+			.setDefaultConnectionConfig(ConnectionConfig.custom()
+				.setConnectTimeout(Timeout.of(properties.connectTimeout()))
+				.setTimeToLive(TimeValue.of(properties.keepAliveDuration()))
+				.build())
+			.setMaxConnPerRoute(properties.maxConnectionsPerRoute())
+			.setMaxConnTotal(properties.maxTotal())
+			.build();
+	}
+
+	@Bean(destroyMethod = "close")
+	public CloseableHttpClient tossHttpClient(
+		PoolingHttpClientConnectionManager connectionManager,
+		TossPaymentProperties properties
+	) {
+		log.info("[TOSS] HTTP 클라이언트 초기화: connectTimeout={}, readTimeout={}, connectionRequestTimeout={}, "
+				+ "maxPerRoute={}, maxTotal={}, keepAlive={}, evictIdle={}",
+			properties.connectTimeout(), properties.readTimeout(), properties.connectionRequestTimeout(),
+			properties.maxConnectionsPerRoute(), properties.maxTotal(),
+			properties.keepAliveDuration(), properties.evictIdleAfter());
+
+		return HttpClients.custom()
+			.setConnectionManager(connectionManager)
+			.setDefaultRequestConfig(RequestConfig.custom()
+				.setConnectionRequestTimeout(Timeout.of(properties.connectionRequestTimeout()))
+				.setResponseTimeout(Timeout.of(properties.readTimeout()))
+				.build())
+			.evictIdleConnections(TimeValue.of(properties.evictIdleAfter()))
+			.evictExpiredConnections()
+			.build();
+	}
+
 	@Bean
-	public RestClient tossPaymentRestClient(TossPaymentProperties properties) {
+	public RestClient tossPaymentRestClient(CloseableHttpClient tossHttpClient, TossPaymentProperties properties) {
 		String encodedSecretKey = Base64.getEncoder()
 			.encodeToString((properties.secretKey() + ":").getBytes(StandardCharsets.UTF_8));
 
+		HttpComponentsClientHttpRequestFactory requestFactory =
+			new HttpComponentsClientHttpRequestFactory(tossHttpClient);
+
 		return RestClient.builder()
+			.requestFactory(requestFactory)
 			.baseUrl(properties.baseUrl())
 			.defaultHeader(HttpHeaders.AUTHORIZATION, "Basic " + encodedSecretKey)
 			.defaultHeader(HttpHeaders.CONTENT_TYPE, MediaType.APPLICATION_JSON_VALUE)
